4ac2abd85c64c905dccee4dfb48aad589bae6d23
Fahim <fahim@Fahims-MacBook-Pro.local>
2021-12-02 18:11:26 +0900

    update with array data update

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,6 +4,8 @@ import { Container, Draggable } from 'react-smooth-dnd';
 import { Rnd } from "react-rnd";
 
 function Grid() {
+  const [sampleData, setSampleData] = useState({start: 1, width: 3, bg: 'red'});
+  const [dragData, setDragData] = useState({x: '', y: ''});
   const [gridData, setGridData] = useState([
     {
       val: 'check 1',
@@ -47,7 +49,7 @@ function Grid() {
           Test page
         </div>
         <div className="w-4/5">
-          check
+          {`position data = ${dragData.x}`}
           <div className="flex mb-1">
             {
               gridHeaderData.map((data, index) => {
@@ -62,19 +64,6 @@ function Grid() {
               })
             }
           </div>
-          {/* {
-            gridData.map(grid => {
-              return (
-              <div className="flex bg-white p-2 border border-red-100">
-                {
-                  grid.map((data, index) => {
-                    return (<div id={index} className={`bg-${data.bg} w-${data.width}/6 text-md`} >{data.val}</div>)
-                  })
-                }
-              </div>
-              )
-            })
-          } */}
           {
             gridData.map((data, index) => {
               return (
@@ -84,36 +73,79 @@ function Grid() {
                       style={{ ...style, backgroundColor: data.bg }}
                       className={`bg-${data.bg} w-${data.width}/6 text-md`}
                       default={{
-                        // x: data.width * 20,
-                        // y: 0,
-                        // width: data.width * 60,
+                        x: (data.start - 1) * 57,
+                        y: -2,
+                        width: data.width * 60,
                         height: 40
                       }}
-                      position={{
-                        x: (data.start-1) * 57,
-                        y: 5
-                      }}
+                      isResizable={{ top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+                      // position={{
+                      //   x: (data.start-1) * 57,
+                      //   y: 5
+                      // }}
                       size={{
+                        height: 40,
                         width: data.width * 58
                       }}
                       minHeight="40"
                       maxHeight="40"
                       dragAxis='x'
+                      // onResizeStop={(e) => {
+                      //   console.log(e)
+                      //   setDragData(e);
+                      // }}
                       onResizeStop={(e) => {
-                        console.log(e)
-                      }}
-                      onDragStop={(e) => {
                         console.log(e.screenX)
                         const temp = gridData;
-                        temp[index] = {...data, start: parseInt((e.screenX/30)%10)};
+                        temp[index] = {...data, width: parseInt((e.x - 200 )/55)+1 };
                         console.log("cheh", temp)
-                        setGridData(temp);
+                        setGridData([...temp]);
                         console.log(gridData)
+                      }}
+                      onDragStop={(e) => {
+                        console.log(e, e.toElement.clientWidth ,e.pageX, parseInt((e.x - 200 )/55)+1  )
+                        const temp = gridData;
+                        // temp[index] = {...data, start: parseInt((e.screenX - 83 )/20)}; 
+                        // setGridData([...temp]);
+                      
                       }}
                     >
                       {/* <div key={index} className={`bg-${data.bg} text-md`} >{data.val}</div> */}
                       day{data.start} to {data.start + data.width}
                     </Rnd>
+                    // <Rnd
+                    //   style={{ ...style, backgroundColor: sampleData.bg }}
+                    //   className={`bg-${sampleData.bg} w-${1}/6 text-md`}
+                    //   default={{
+                    //     // x: data.width * 20,
+                    //     // y: 0,
+                    //     // width: data.width * 60,
+                    //     height: 40
+                    //   }}
+                    //   position={{
+                    //     x: (sampleData.start-1) * 57,
+                    //     y: 5
+                    //   }}
+                    //   size={{
+                    //     width: sampleData.width * 58
+                    //   }}
+                    //   minHeight="40"
+                    //   maxHeight="40"
+                    //   dragAxis='x'
+                    //   onResizeStop={(e) => {
+                    //     console.log(e)
+                    //   }}
+                    //   onDragStop={(e) => {
+                    //     // console.log(e.screenX)
+                    //     // const temp = gridData;
+                    //     // temp[index] = {...data, start: parseInt((e.screenX/30)%10)};
+                    //     // console.log("cheh", temp)
+                    //     setSampleData({start: parseInt((e.screenX/30)%20), width: 3});
+                    //     console.log(gridData)
+                    //   }}
+                    // >
+                    //   ff
+                    // </Rnd>
                   }
                 </div>
               )
